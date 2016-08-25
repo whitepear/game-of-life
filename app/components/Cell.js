87b46b3b560ddb08375360535c57d46c40c8e929
John Doe <webdevnorth@gmail.com>
2016-08-25 20:48:58 +0100

    created Cell component with boilerplate

---
+++ app/components/Cell.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+function Cell (props) {
+	return (
+		<div>Cell</div>
+	)
+}
+
+Cell.propTypes = {
+	
+};
+
+module.exports = Cell;
