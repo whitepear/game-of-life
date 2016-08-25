23c91cdcf7fda0faa4fc6cc7138cd92538e97a79
John Doe <webdevnorth@gmail.com>
2016-08-25 20:49:18 +0100

    created Grid and MainContainers with boilerplate

---
+++ app/containers/GridContainer.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Cell = require('../components/Cell.js');
+
+var GridContainer = React.createClass({
+	render: function () {
+		return (
+			<Cell />
+		)
+	}
+});
+
+module.exports = GridContainer;
---
+++ app/containers/MainContainer.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var GridContainer = require('./GridContainer.js');
+
+var MainContainer = React.createClass({
+	render: function () {
+		return (
+			<GridContainer />
+		)		
+	}
+});
+
+module.exports = MainContainer;
