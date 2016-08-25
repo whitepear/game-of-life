ab8a86dbb0cc5ed2fa8eccb00d661da247afc56b
John Doe <webdevnorth@gmail.com>
2016-08-25 23:24:15 +0100

    implemented gridColumns state

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -4,12 +4,13 @@ var GridContainer = require('./GridContainer.js');
 var MainContainer = React.createClass({
 	getInitialState: function () {
 		return {
-			gridRows: 8
+			gridRows: 8,
+			gridColumns: 5
 		};
 	},
 	render: function () {
 		return (
-			<GridContainer gridRows={this.state.gridRows} />
+			<GridContainer gridRows={this.state.gridRows} gridColumns={this.state.gridColumns} />
 		)		
 	}
 });
