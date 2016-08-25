e4e9075e6710f123898bfa25a86cc4ea8976b9d2
John Doe <webdevnorth@gmail.com>
2016-08-25 22:12:50 +0100

    implemented gridArray creation function

--- app/containers/GridContainer.js
+++ app/containers/GridContainer.js
@@ -2,6 +2,15 @@ var React = require('react');
 var Cell = require('../components/Cell.js');
 
 var GridContainer = React.createClass({
+	createGridArray: function (rows) {
+		// this function creates a two-dimensional array
+		// array height is dependent on the value of rows
+		var grid = [];
+		for (var i = 0; i < rows; i++) {
+			grid[i] = [];
+		}
+		return grid;
+	},
 	render: function () {
 		return (
 			<Cell />
