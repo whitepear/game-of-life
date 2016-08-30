18910124c3c21bc781e92a662bbc00102b0e9acd
John Doe <webdevnorth@gmail.com>
2016-08-30 18:21:40 +0100

    updated PropTypes and JSX in Cell to show row/column origin in populatedGrid

--- app/components/Cell.js
+++ app/components/Cell.js
@@ -7,12 +7,14 @@ function Cell (props) {
 		width: (100/props.gridColumns) + '%'
 	};
 	return (
-		<div className={props.isAlive ? 'alive' : 'dead'} style={cellDimensions}></div>
+		<div className={props.isAlive ? 'alive' : 'dead'} style={cellDimensions} data-row-origin={props.cellRowOrigin} data-column-origin={props.cellColumnOrigin}></div>
 	)
 }
 
 Cell.propTypes = {	
 	isAlive: PropTypes.number.isRequired,
+	cellRowOrigin: PropTypes.number.isRequired,
+	cellColumnOrigin: PropTypes.number.isRequired,
 	gridRows: PropTypes.number.isRequired,
 	gridColumns: PropTypes.number.isRequired
 };
