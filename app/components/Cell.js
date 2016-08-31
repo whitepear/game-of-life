b12e354910d5ec307b91ff30239e428eb4eef646
John Doe <webdevnorth@gmail.com>
2016-08-31 17:02:14 +0100

    refactored cell to stateful component, uses shouldComponentUpdate

--- app/components/Cell.js
+++ app/components/Cell.js
@@ -1,23 +1,27 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
-function Cell (props) {
-	var cellDimensions = {
-		height: (100/props.gridRows) + '%',
-		width: (100/props.gridColumns) + '%'
-	};
-	return (
-		<div className={props.isAlive ? 'alive' : 'dead'} style={cellDimensions} data-row-origin={props.cellRowOrigin} data-column-origin={props.cellColumnOrigin} onClick={props.onChangeCell}></div>
-	)
-}
-
-Cell.propTypes = {	
-	isAlive: PropTypes.number.isRequired,
-	cellRowOrigin: PropTypes.number.isRequired,
-	cellColumnOrigin: PropTypes.number.isRequired,
-	onChangeCell: PropTypes.func.isRequired,
-	gridRows: PropTypes.number.isRequired,
-	gridColumns: PropTypes.number.isRequired
-};
+var Cell = React.createClass({
+	propTypes: {
+		isAlive: PropTypes.number.isRequired,
+		cellRowOrigin: PropTypes.number.isRequired,
+		cellColumnOrigin: PropTypes.number.isRequired,
+		onChangeCell: PropTypes.func.isRequired,
+		gridRows: PropTypes.number.isRequired,
+		gridColumns: PropTypes.number.isRequired
+	},
+	shouldComponentUpdate: function (nextProps, nextState) {
+		return !(nextProps.isAlive === this.props.isAlive);
+	},
+	render: function () {
+		var cellDimensions = {
+			height: (100/this.props.gridRows) + '%',
+			width: (100/this.props.gridColumns) + '%'
+		};
+		return (
+			<div className={this.props.isAlive ? 'alive' : 'dead'} style={cellDimensions} data-row-origin={this.props.cellRowOrigin} data-column-origin={this.props.cellColumnOrigin} onClick={this.props.onChangeCell}></div>
+		)
+	}
+});
 
 module.exports = Cell;
