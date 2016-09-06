7f9646429649bf217bd573a2006af4e8e0afe792
John Doe <webdevnorth@gmail.com>
2016-09-06 20:24:46 +0100

    styled buttons for grid controls

--- app/components/GridControls.js
+++ app/components/GridControls.js
@@ -4,12 +4,12 @@ var PropTypes = React.PropTypes;
 function GridControls (props) {
 	return (
 		<div className="text-center">			
-			<button type="button" className="button button-play" onClick={props.onToggleRunning}>
+			<button type="button" className="btn button button-play" onClick={props.onToggleRunning}>
 				{props.isGridRunning ? <i className="fa fa-pause" aria-hidden="true"></i> : <i className="fa fa-play" aria-hidden="true"></i>}
 			</button>
-			<button type="button" className="button button-clear" onClick={props.onClearGrid}>Clear Grid</button>
-			<button type="button" className="button button-random" onClick={props.onRandomiseGrid}><i className="fa fa-random" aria-hidden="true"></i></button>
-			<button type="button" className="button button-step" onClick={props.nextGrid} disabled={props.isGridRunning}><i className="fa fa-step-forward" aria-hidden="true"></i></button>			
+			<button type="button" className="btn button button-clear" onClick={props.onClearGrid}><i className="fa fa-stop" aria-hidden="true"></i></button>
+			<button type="button" className="btn button button-random" onClick={props.onRandomiseGrid}><i className="fa fa-random" aria-hidden="true"></i></button>
+			<button type="button" className="btn button button-step" onClick={props.nextGrid} disabled={props.isGridRunning}><i className="fa fa-step-forward" aria-hidden="true"></i></button>			
 		</div>			
 	)
 }
